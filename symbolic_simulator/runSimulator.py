0b01b2758abbee3b9678b56b8d1ca9382c06da3a
Juliano-hub <juliano.buss@gmail.com>
2024-06-07 17:30:31 -0300

    tentando fazer a toffoli

--- symbolic_simulator/runSimulator.py
+++ symbolic_simulator/runSimulator.py
@@ -7,14 +7,22 @@ simulator = Simulator.Simulator()
 #simulator.createState(4, [x])
 #simulator.createState(2, [x,y])
 #simulator.createState(1, [x])
-simulator.createState(3, [x,y,z])
+#simulator.createState(3, [x,y,z])
+
+
+#simulator.createState(5, [x,x,y,0,0])
+simulator.createState(3, [x,y,0])
 simulator.printNonZeroPosState()
 
 print ("#######")
 
 #simulator.executeCircuit("C-X,0-3;X,0;H,0;H,0")
-simulator.executeCircuit("C-SX,1-2;C-SX,0-2")
+#simulator.executeCircuit("C-SX,1-2;C-SX,0-2")
 #simulator.executeCircuit("V,0")
+#simulator.executeCircuit("H,0")
+
+#circuito pais e filho
+#simulator.executeCircuit("Toffoli, 0-1-2")
 simulator.printNonZeroPosState()
 
 print ("####### Measure #######")
