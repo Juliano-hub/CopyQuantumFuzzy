6e27ff22013a121f361e2b1a219509e4f6714452
Juliano-hub <juliano.buss@gmail.com>
2024-06-20 15:20:15 -0300

    Arrumando descrições

--- symbolic_simulator/runSimulator.py
+++ symbolic_simulator/runSimulator.py
@@ -11,8 +11,8 @@ simulator = Simulator.Simulator()
 
 
 #simulator.createState(5, [x,x,y,0,0])
-simulator.createState(2, [x,y])
-#simulator.createState(5, [x,x,0,y,0])
+#simulator.createState(2, [x,y])
+simulator.createState(5, [x,x,0,y,0])
 #simulator.createState(1, [1])
 simulator.printNonZeroPosState()
 
@@ -31,16 +31,13 @@ print ("#######")
 
 #------------------------------------
 # C-NOT:
-#C - lista de portas/qubits - controles
-#"C-X,0-1" - porta X no qubit 0 / 1 é a parte do controle - controle em 1
-simulator.executeCircuit("C-X,0-1")
+#"C-X,CONTROLE-ALVO"
+#simulator.executeCircuit("C-X,0-1") - se qubit 0 tem valor 1, aplica X no qubit 1
+#simulator.executeCircuit("C-X,1-0") - se qubit 1 tem valor 1, aplica X no qubit 0
 #------------------------------------
 
-#circuito pais e filho
-#simulator.executeCircuit("C-X,0-1,2")
-
-#simulator.executeCircuit("C-X,0-1,2;C-SX,2-4;C-SX,3-4")
-#simulator.executeCircuit("C-X,0-1,2;C-SX,3-4;C-SX,2-4")
+# Circuito pai e filho modal
+simulator.executeCircuit("C-X,0-1,2;C-SX,3-4;C-SX,2-4")
 
 simulator.printNonZeroPosState()
 
