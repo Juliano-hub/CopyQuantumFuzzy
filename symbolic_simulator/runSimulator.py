94a27959bb78fb45b221168ad3ea90a2c57a4f98
Juliano-hub <juliano.buss@gmail.com>
2024-06-21 12:37:30 -0300

    Atualizando circ final

--- symbolic_simulator/runSimulator.py
+++ symbolic_simulator/runSimulator.py
@@ -11,9 +11,11 @@ simulator = Simulator.Simulator()
 #                         [2,1,0]
 #simulator.createState(2, [x,y])
 # simulator.createState(5, [x,x])
+
 simulator.createState(5, [x,x,0,y,0])
 #simulator.createState(5, [x,x,0,y,0])
 #simulator.createState(5, [4,3,2,1,0])
+
 #simulator.createState(1, [1])
 simulator.printNonZeroPosState()
 
@@ -22,13 +24,13 @@ print ("#######")
 #simulator.executeCircuit("C-X,0-3;X,0;H,0;H,0")
 #simulator.executeCircuit("V,0")
 #simulator.executeCircuit("H,0")
+
 #------------------------------------
 #Toffoli:
 #simulator.executeCircuit("C-X,0-1,2")
+#C-X,3-1,2,0 -> Applies X gate on qubit 3 if qubits 1 and 2 are 1 and 0 respectively
 #o qubit 0 é o ultimo da lista, menos significativo
 #qubit 1 é o y e qubit 2 é o x
-#------------------------------------
-#simulator.executeCircuit("C-X,2-3,4") - Toffoli para 3 qubits
 #------------------------------------
 # C-NOT:
 #"C-X,CONTROLE-ALVO"
@@ -37,11 +39,13 @@ print ("#######")
 #simulator.executeCircuit("C-X,1-0") 
 # se qubit 1 tem valor 1, aplica X no qubit 0
 #------------------------------------
+
 #simulator.executeCircuit("C-SX,0-2;C-SX,1-2")
 #simulator.executeCircuit("C-SX,0-1")
 
 # Circuito pais e filho modal: x^2 é armazenado no qubit 3, realiza a CRaizQuadrada entre qubit y e filho, e depois CRaizQuadrada pai^2 e filho
-simulator.executeCircuit("C-X,2-3,4;C-SX,0-2;C-SX,0-1")
+#simulator.executeCircuit("X,3")
+simulator.executeCircuit("X,3;C-X,2-3,4;C-SX,0-2;C-SX,0-1")
 # cnot C-X,0-2 para 5 qubits 00[Controle 0]0[Alvo 0]
 # cnot C-X,0-1 para 5 qubits 000[Controle 0][Alvo 0]
 
